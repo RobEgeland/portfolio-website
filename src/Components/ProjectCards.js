f8939275660b6cce19d129ce3f5c4631d8fc9751
Robert Egeland <robegeland@yahoo.com>
2023-05-04 18:46:52 -0500

    adds single prject

--- src/Components/ProjectCards.js
+++ src/Components/ProjectCards.js
@@ -4,10 +4,11 @@ const ProjectCards = () => {
   return (
         <div class="card">
             <div class="pricing-block-content">
-                <p class="pricing-plan">Project name</p>
+                <p class="pricing-plan">Synth-etic</p>
+                <p className='proj-desc'>A web based dual oscillator synthesizer</p>
                 <div class="price-value" data-currency="$ USD" data-currency-simple="USD">
                     <button onClick={() => window.open("https://synth-etic.onrender.com")} class="btn">Website</button>
-                    <button class="btn">Github</button>
+                    <button onClick={() => window.open("https://github.com/RobEgeland/Synth-etic")} class="btn">Github</button>
                 </div>
             </div>
         </div>
